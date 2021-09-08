fb7bc2cf0a29ef0fb855a763a6d482be75677702
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-09-08 13:52:48 -0700

    Connect to database
    Import both dotenv and sequelize into server.js, and use those packages
    to login to the database specified in the .env file

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import Sequelize from 'sequelize';
 
+dotenv.config();
 const app = express();
 const port = 3000;
+const sequelize = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`,
+);
 
 app.use(express.json());
 
 app.post('/blogs', (req, res) => res.sendStatus(200));
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connection established');
+  }).catch((err) => {
+    console.error('Database connection failed:', err);
+  });
 
 app.listen(port, () => console.log(`blog-api listening on port ${port}`));
